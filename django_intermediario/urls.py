b96bfb101f06266678c98b40ae65c38c2e9a82e3
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-02-07 23:47:33 -0300

    add templates

--- django_intermediario/urls.py
+++ django_intermediario/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', views.login)
 ]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/leandrotavares/Documents/Dev/django_intermediario/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
