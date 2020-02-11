02127b87d0c02f940055bc5c09ae033ae4b1920f
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-02-10 22:35:35 -0300

    Add model Address

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+STATE_CHOICE = (
+    ('CE', 'Ceara'),
+    ('SP', 'São Paulo'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RS', 'Rio Grande do Sul')
+)
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=255)
+    address_complement = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, choices=STATE_CHOICE)
+    country = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+    def __str__(self):
+        return '%s %s %s' % (self.address, self.city, self.state)
