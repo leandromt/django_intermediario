254b67796affa3bc893ab08e0adeda761ec260c0
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-02-12 22:34:57 -0300

    Add create address

--- my_app/views.py
+++ my_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.contrib.auth.decorators import login_required
-from .models import Address
+from .models import Address, STATE_CHOICE
 
 
 def login(request: HttpRequest):
@@ -46,5 +46,16 @@ def address_list(request):
 
 @login_required(login_url='/login')
 def address_create(request):
-    addresses = Address.objects.all().filter().order_by('address')
-    return render(request, 'my_app/address/create.html', {'addresses': addresses})
+    if request.method == 'GET':
+        return render(request, 'my_app/address/create.html', {'states': STATE_CHOICE})
+
+    Address.objects.create(
+        address=request.POST.get('address'),
+        address_complement=request.POST.get('address'),
+        city=request.POST.get('city'),
+        state=request.POST.get('state'),
+        country=request.POST.get('country'),
+        user=request.user
+    )
+
+    return redirect('/addresses/')
